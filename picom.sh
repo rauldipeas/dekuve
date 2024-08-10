c37f0a3c0a71f70887ada98a51940dcd7ce04a8c
Raul Dipeas <rauldipeas@disroot.org>
2024-08-10 13:08:29 -0300

    ^picom

--- picom.sh
+++ picom.sh
@@ -22,6 +22,7 @@ sudo apt install -y\
     libxcb-shape0-dev\
     libxcb-util-dev\
     libxcb-xfixes0-dev\
+    libxext-dev\
     meson\
     ninja-build\
     uthash-dev
@@ -31,11 +32,11 @@ git submodule update --init --recursive
 meson --buildtype=release . build
 ninja -C build
 mkdir -p picom/DEBIAN picom/usr/bin
-PICOM_TAG=`echo $(curl -su $GH_API_AUTH https://api.github.com/repos/yshui/picom/releases|grep tag|grep -v Next|head -n1|cut -d '"' -f4|sed 's/https:\/\/github.com\/yshui\/picom\/releases\/tag\///g'|sed 's/v//g')`
-PICOM_VER=`echo "$(git describe --always --dirty)"-"$(git log -1 --date=short --pretty=format:%cd)"|sed 's/v//g'|sed 's/_/-/g'`
+PICOM_TAG=`echo $(curl -s https://api.github.com/repos/yshui/picom/releases|grep tag|grep -v Next|head -n1|cut -d '"' -f4|sed 's/https:\/\/github.com\/yshui\/picom\/releases\/tag\///g'|sed 's/v//g')`
+#PICOM_VER=`echo "$(git describe --always --dirty)"-"$(git log -1 --date=short --pretty=format:%cd)"|sed 's/v//g'|sed 's/_/-/g'`
 cat <<EOF |tee picom/DEBIAN/control
 Package: picom
-Version: $PICOM_TAG-$PICOM_VER
+Version: $PICOM_TAG
 Architecture: amd64
 Maintainer: Yuxuan Shui <yshuiv7@gmail.com>
 Depends: python3, libc6 (>= 2.29), libconfig9, libdbus-1-3 (>= 1.9.14), libev4 (>= 1:4.04), libgl1, libpcre3, libpixman-1-0 (>= 0.25.2), libx11-6, libx11-xcb1 (>= 2:1.6.12), libxcb-composite0, libxcb-damage0, libxcb-glx0, libxcb-image0 (>= 0.2.1), libxcb-present0, libxcb-randr0 (>= 1.10), libxcb-render-util0, libxcb-render0 (>= 1.12), libxcb-shape0, libxcb-sync1, libxcb-xfixes0, libxcb-xinerama0, libxcb1 (>= 1.9.2)
