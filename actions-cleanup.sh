5b54249b88ae185784a0e8505f1aede8bf11e58a
Raul Dipeas <rauldipeas@disroot.org>
2024-08-26 00:43:38 -0300

    +actions-cleanup ^dekuve

---
+++ actions-cleanup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+# Actions cleanup
+PROJECT='rauldipeas/dekuve'
+KEEP=5
+gh api repos/"$PROJECT"/actions/runs --paginate -q '.workflow_runs[]|select(.head_branch != "any")|"\(.id)"'|tail -n+$((KEEP+1))|xargs -n1 -I % gh api repos/"$PROJECT"/actions/runs/% -X DELETE
