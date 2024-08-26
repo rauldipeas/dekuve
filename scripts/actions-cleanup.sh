7007351d858bbe4c6e17051a4d6b442fd3b998ea
Raul Dipeas <rauldipeas@disroot.org>
2024-08-26 02:10:29 -0300

    ^actions-cleanup

--- scripts/actions-cleanup.sh
+++ scripts/actions-cleanup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+# Install
+wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg|sudo -A gpg --dearmor -o /etc/apt/trusted.gpg.d/githubcli-archive-keyring.gpg>/dev/null
+echo 'deb [arch=amd64] https://cli.github.com/packages stable main'|sudo tee /etc/apt/sources.list.d/github-cli.list>/dev/null
+sudo apt update
+sudo apt install -y gh
+
 # Actions cleanup
 PROJECT='rauldipeas/dekuve'
 KEEP=5
