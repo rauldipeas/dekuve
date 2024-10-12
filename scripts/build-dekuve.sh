6b2bd78c7066ccd9ebc2b5322f397d3038d1126a
Raul Dipeas <rauldipeas@disroot.org>
2024-10-11 22:43:12 -0300

    ^build-dekuve

--- scripts/build-dekuve.sh
+++ scripts/build-dekuve.sh
@@ -10,7 +10,7 @@ rm -f debian-archive-keyring*.deb live-build*.deb>/dev/null
 wget -q --show-progress http://ftp.us.debian.org/debian/pool/main/d/debian-archive-keyring/debian-archive-keyring_2023.3+deb12u1_all.deb
 wget -q --show-progress http://ftp.us.debian.org/debian/pool/main/l/live-build/live-build_20230502_all.deb
 sudo -A apt install -y ./debian-archive-keyring*.deb ./live-build*.deb
-sudo -A rm -rfv /tmp/dekuve
+sudo -A rm -rf /tmp/dekuve
 mkdir -p /tmp/dekuve
 cd /tmp/dekuve
 lb config\
