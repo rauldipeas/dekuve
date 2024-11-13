383a5bdf8932109a706e85124c9d682d38083606
Raul Dipeas <github.rauldipeas@lock.email>
2024-11-13 14:58:16 -0300

    ^blur-effect ^picom

--- scripts/picom.sh
+++ scripts/picom.sh
@@ -31,17 +31,17 @@ sudo -A apt install -y\
     ninja-build\
     pkg-config\
     uthash-dev
-git clone --depth 1 --branch v12.2 https://github.com/yshui/picom
+PICOM_TAG=`echo $(curl -s https://api.github.com/repos/yshui/picom/releases|grep tag|grep -v Next|head -n1|cut -d '"' -f4|sed 's/https:\/\/github.com\/yshui\/picom\/releases\/tag\///g'|sed 's/v//g')`
+#PICOM_VER=`echo "$(git describe --always --dirty)"-"$(git log -1 --date=short --pretty=format:%cd)"|sed 's/v//g'|sed 's/_/-/g'`
+git clone --depth 1 --branch $PICOM_TAG https://github.com/yshui/picom
 cd picom
 git submodule update --init --recursive
 meson --buildtype=release . build
 ninja -C build
 mkdir -p picom/DEBIAN picom/usr/bin
-#PICOM_TAG=`echo $(curl -s https://api.github.com/repos/yshui/picom/releases|grep tag|grep -v Next|head -n1|cut -d '"' -f4|sed 's/https:\/\/github.com\/yshui\/picom\/releases\/tag\///g'|sed 's/v//g')`
-#PICOM_VER=`echo "$(git describe --always --dirty)"-"$(git log -1 --date=short --pretty=format:%cd)"|sed 's/v//g'|sed 's/_/-/g'`
 cat <<EOF |tee picom/DEBIAN/control
 Package: picom
-Version: 12.2
+Version: $PICOM_TAG
 Architecture: amd64
 Maintainer: Yuxuan Shui <yshuiv7@gmail.com>
 Depends: python3, libc6 (>= 2.29), libconfig9, libdbus-1-3 (>= 1.9.14), libev4 (>= 1:4.04), libgl1, libpcre3, libpixman-1-0 (>= 0.25.2), libx11-6, libx11-xcb1 (>= 2:1.6.12), libxcb-composite0, libxcb-damage0, libxcb-glx0, libxcb-image0 (>= 0.2.1), libxcb-present0, libxcb-randr0 (>= 1.10), libxcb-render-util0, libxcb-render0 (>= 1.12), libxcb-shape0, libxcb-sync1, libxcb-xfixes0, libxcb-xinerama0, libxcb1 (>= 1.9.2)
